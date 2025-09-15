a07133756607e1d28f72e522b751ab68d5caa04a
Ashwani Kumar Singh <ashwanisingh8713@gmail.com>
2025-09-15 05:37:24 +0530

    Added pages

---
+++ site/src/jsMain/kotlin/com/mano/ashwa/pages/CoverLetter.kt
@@ -0,0 +1,10 @@
+package com.mano.ashwa.pages
+
+import androidx.compose.runtime.Composable
+import com.varabyte.kobweb.core.Page
+
+@Page("/coverletter")
+@Composable
+fun CoverLetter() {
+    // You can add content here for the My CoverLetter page
+}
---
+++ site/src/jsMain/kotlin/com/mano/ashwa/pages/Experiences.kt
@@ -0,0 +1,12 @@
+package com.mano.ashwa.pages
+
+import androidx.compose.runtime.Composable
+import com.mano.ashwa.navigation.Screen
+import com.varabyte.kobweb.core.Page
+
+
+@Page("/experience")
+@Composable
+fun Experiences() {
+    // You can add content here for the My Experience page
+}
