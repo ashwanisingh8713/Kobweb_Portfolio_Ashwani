fa840e799d993594d4f69673162c47eff36043a3
Abhishek Verma <abhishek@Abhisheks-MacBook-Air.local>
2025-10-23 15:36:19 +0530

    fixed the UI

--- site/src/jsMain/kotlin/com/mano/ashwa/pages/Index.kt
+++ site/src/jsMain/kotlin/com/mano/ashwa/pages/Index.kt
@@ -1,13 +1,21 @@
 package com.mano.ashwa.pages
 
 import androidx.compose.runtime.Composable
+import com.mano.ashwa.components.layouts.PageLayoutData
+import com.mano.ashwa.sections.Banner
 import com.varabyte.kobweb.compose.css.StyleVariable
-import com.varabyte.kobweb.compose.foundation.layout.Row
 import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.graphics.Color
-import com.varabyte.kobweb.compose.ui.modifiers.*
-import com.varabyte.kobweb.compose.ui.toAttrs
+import com.varabyte.kobweb.compose.ui.modifiers.backgroundColor
+import com.varabyte.kobweb.compose.ui.modifiers.borderRadius
+import com.varabyte.kobweb.compose.ui.modifiers.boxShadow
+import com.varabyte.kobweb.compose.ui.modifiers.fillMaxWidth
+import com.varabyte.kobweb.compose.ui.modifiers.gap
+import com.varabyte.kobweb.compose.ui.modifiers.height
+import com.varabyte.kobweb.compose.ui.modifiers.margin
+import com.varabyte.kobweb.compose.ui.modifiers.width
 import com.varabyte.kobweb.core.Page
+import com.varabyte.kobweb.core.PageContext
 import com.varabyte.kobweb.core.data.add
 import com.varabyte.kobweb.core.init.InitRoute
 import com.varabyte.kobweb.core.init.InitRouteContext
@@ -15,17 +23,8 @@ import com.varabyte.kobweb.core.layout.Layout
 import com.varabyte.kobweb.silk.style.CssStyle
 import com.varabyte.kobweb.silk.style.base
 import com.varabyte.kobweb.silk.style.breakpoint.Breakpoint
-import com.varabyte.kobweb.silk.style.breakpoint.displayIfAtLeast
-import com.varabyte.kobweb.silk.style.toModifier
-import com.varabyte.kobweb.silk.theme.colors.ColorMode
-import com.varabyte.kobweb.silk.theme.colors.ColorPalettes
 import org.jetbrains.compose.web.css.cssRem
-import org.jetbrains.compose.web.css.fr
 import org.jetbrains.compose.web.css.vh
-import org.jetbrains.compose.web.dom.Div
-import com.mano.ashwa.components.layouts.PageLayoutData
-import com.mano.ashwa.toSitePalette
-import com.mano.ashwa.sections.Banner
 
 // Container that has a tagline and grid on desktop, and just the tagline on mobile
 val HeroContainerStyle = CssStyle {
@@ -49,47 +48,21 @@ val HomeGridCellStyle = CssStyle.base {
         .borderRadius(1.cssRem)
 }
 
-@Composable
-private fun GridCell(color: Color, row: Int, column: Int, width: Int? = null, height: Int? = null) {
-    Div(
-        HomeGridCellStyle.toModifier()
-            .setVariable(GridCellColorVar, color)
-            .gridItem(row, column, width, height)
-            .toAttrs()
-    )
-}
+
 
 
 @InitRoute
 fun initHomePage(ctx: InitRouteContext) {
-    ctx.data.add(PageLayoutData("Home"))
+    ctx.data.add(PageLayoutData("Ashwani Kumar"))
 }
 
 @Page
 @Layout(".components.layouts.PageLayout")
 @Composable
-fun HomePage() {
-    Row(HeroContainerStyle.toModifier()) {
-        // Render the site banner (hero) section
-        Banner()
-
-        Div(
-            HomeGridStyle
-            .toModifier()
-            .displayIfAtLeast(Breakpoint.MD)
-            .grid {
-                rows { repeat(3) { size(1.fr) } }
-                columns { repeat(5) { size(1.fr) } }
-            }
-            .toAttrs()
-        ) {
-            val sitePalette = ColorMode.current.toSitePalette()
-            GridCell(sitePalette.brand.primary, 1, 1, 2, 2)
-            GridCell(ColorPalettes.Monochrome._600, 1, 3)
-            GridCell(ColorPalettes.Monochrome._100, 1, 4, width = 2)
-            GridCell(sitePalette.brand.accent, 2, 3, width = 2)
-            GridCell(ColorPalettes.Monochrome._300, 2, 5)
-            GridCell(ColorPalettes.Monochrome._800, 3, 1, width = 5)
-        }
-    }
+fun HomePage(ctx: PageContext) {
+    // When @Layout is used, then mentioned "PageLayout" UI comes automatically in calling page.
+    // the "content" parameter which is here as lambda, gets remaining UI of the page
+    // Below full code is referred from PageLayout.kt -> PageLayout function parameter 'content'
+    // here whatever is written those will go inside "content" parameter, because it has last parameter as Lambda function
+    Banner()
 }
